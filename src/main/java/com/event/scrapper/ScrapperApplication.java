68b4ae4a34ca4db8351c637651e7dba628cb206d
Patryk <chojnackipatryk96@gmail.com>
2022-07-14 14:51:22 +0200

    Initial commit with empty project

---
+++ src/main/java/com/event/scrapper/ScrapperApplication.java
@@ -0,0 +1,13 @@
+package com.event.scrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrapperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/event/scrapper/ScrapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.event.scrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
