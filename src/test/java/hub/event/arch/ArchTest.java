aaa2b2f641cfa4f5ee86069c6aebe9a0bd95c452
Pawel <synk@poczta.onet.pl>
2022-08-20 20:57:26 +0200

    fix: 37 Arch tests bug fix
    - added missed condition to enable classes to depend on classes
      from the same package and sub-packages
    - test classes excluded from arch test

--- src/test/java/hub/event/arch/ArchTest.java
+++ src/test/java/hub/event/arch/ArchTest.java
@@ -2,6 +2,8 @@ package hub.event.arch;
 
 import com.tngtech.archunit.core.domain.JavaClasses;
 import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.core.importer.ImportOption;
+import com.tngtech.archunit.core.importer.ImportOptions;
 import com.tngtech.archunit.lang.ArchRule;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,25 +15,25 @@ class ArchTest {
 
     public static final String HUB_EVENT = "hub.event";
     @SuppressWarnings("SpellCheckingInspection")
-    public static final String MAIL_MODULE = HUB_EVENT + ".mailmodule";
+    public static final String MAIL_MODULE = HUB_EVENT + ".mailmodule..";
     @SuppressWarnings("SpellCheckingInspection")
-    public static final String ADMIN_API = HUB_EVENT + ".adminapi";
-    public static final String AUTH = HUB_EVENT + ".auth";
-    public static final String EVENTS = HUB_EVENT + ".events";
-    public static final String NEWSLETTER = HUB_EVENT + ".newsletter";
-    public static final String USER = HUB_EVENT + ".users";
+    public static final String ADMIN_API = HUB_EVENT + ".adminapi..";
+    public static final String AUTH = HUB_EVENT + ".auth..";
+    public static final String EVENTS = HUB_EVENT + ".events..";
+    public static final String NEWSLETTER = HUB_EVENT + ".newsletter..";
+    public static final String USER = HUB_EVENT + ".users..";
     @SuppressWarnings("SpellCheckingInspection")
-    public static final String USER_API = HUB_EVENT + ".usersapi";
-    public static final String STATS = HUB_EVENT + ".statistics";
-    public static final String SCRAPERS = HUB_EVENT + ".scrapers";
-    public static final String SCRAPERS_EBILET = HUB_EVENT + ".scrapers.ebilet";
+    public static final String USER_API = HUB_EVENT + ".usersapi..";
+    public static final String STATS = HUB_EVENT + ".statistics..";
+    public static final String SCRAPERS = HUB_EVENT + ".scrapers..";
+    public static final String SCRAPERS_EBILET = HUB_EVENT + ".scrapers.ebilet..";
     @SuppressWarnings("SpellCheckingInspection")
-    public static final String SCRAPERS_EMPIKBILET = HUB_EVENT + ".scrapers.empikbilet";
+    public static final String SCRAPERS_EMPIKBILET = HUB_EVENT + ".scrapers.empikbilet..";
     @SuppressWarnings("SpellCheckingInspection")
-    public static final String SCRAPERS_GOINGAPP = HUB_EVENT + ".scrapers.goingapp";
+    public static final String SCRAPERS_GOINGAPP = HUB_EVENT + ".scrapers.goingapp..";
     @SuppressWarnings("SpellCheckingInspection")
-    public static final String SCRAPERS_KUPBILECIK = HUB_EVENT + ".scrapers.kupbilecik";
-    public static final String SCRAPERS_PROANIMA = HUB_EVENT + ".scrapers.proanima";
+    public static final String SCRAPERS_KUPBILECIK = HUB_EVENT + ".scrapers.kupbilecik..";
+    public static final String SCRAPERS_PROANIMA = HUB_EVENT + ".scrapers.proanima..";
     public static final String JAVA = "..java..";
     public static final String SPRING = "..org.springframework..";
     public static final String JAVAX = "..javax..";
@@ -42,14 +44,16 @@ class ArchTest {
     @Test
     @DisplayName("Test for Scrapers Module")
     void givenScrapersModule_thenCheckDependencyOnlyOnEvents() {
-        JavaClasses javaClasses = new ClassFileImporter().importPackages(HUB_EVENT);
+        //Import classes from application and omit test classes
+        JavaClasses javaClasses = new ClassFileImporter(new ImportOptions()
+                .with(new ImportOption.DoNotIncludeTests())).importPackages(HUB_EVENT);
 
         ArchRule archRule = classes()
                 .that().resideInAPackage(SCRAPERS).and().resideOutsideOfPackages(SCRAPERS_EBILET, SCRAPERS_EMPIKBILET, SCRAPERS_GOINGAPP,
                         SCRAPERS_KUPBILECIK, SCRAPERS_PROANIMA)
                 .should().onlyDependOnClassesThat()
                 .resideInAnyPackage(JAVA, JAVAX, SPRING,
-                        EVENTS);
+                        EVENTS, SCRAPERS);
 
         archRule.check(javaClasses);
 
@@ -58,7 +62,9 @@ class ArchTest {
     @Test
     @DisplayName("Test for Scrapers Internal Implementation Modules")
     void givenScrapersImplementationModule_thenCheckDependencyOnlyOnAbstractScraperAndScrapingLibraries() {
-        JavaClasses javaClasses = new ClassFileImporter().importPackages(HUB_EVENT);
+        //Import classes from application and omit test classes
+        JavaClasses javaClasses = new ClassFileImporter(new ImportOptions()
+                .with(new ImportOption.DoNotIncludeTests())).importPackages(HUB_EVENT);
 
         ArchRule archRule = classes()
                 .that()
@@ -75,13 +81,16 @@ class ArchTest {
     @Test
     @DisplayName("Test for Event Module")
     void givenEventModule_thenCheckDoesNotDependOnAnyOtherModuleFromApp() {
-        JavaClasses javaClasses = new ClassFileImporter().importPackages(HUB_EVENT);
+        //Import classes from application and omit test classes
+        JavaClasses javaClasses = new ClassFileImporter(new ImportOptions()
+                .with(new ImportOption.DoNotIncludeTests())).importPackages(HUB_EVENT);
 
         ArchRule archRule = classes()
                 .that()
                 .resideInAPackage(EVENTS)
                 .should().onlyDependOnClassesThat()
-                .resideInAnyPackage(JAVA, JAVAX, SPRING);
+                .resideInAnyPackage(JAVA, JAVAX, SPRING,
+                        EVENTS);
         archRule.check(javaClasses);
 
     }
@@ -89,10 +98,81 @@ class ArchTest {
     @Test
     @DisplayName("Test for User Module")
     void givenUSerModule_thenCheckDependencies() {
-        JavaClasses javaClasses = new ClassFileImporter().importPackages(HUB_EVENT);
+        //Import classes from application and omit test classes
+        JavaClasses javaClasses = new ClassFileImporter(new ImportOptions()
+                .with(new ImportOption.DoNotIncludeTests())).importPackages(HUB_EVENT);
+
         ArchRule archRule = classes()
                 .that()
                 .resideInAPackage(USER)
+                .should().onlyDependOnClassesThat()
+                .resideInAnyPackage(JAVA, JAVAX, SPRING,
+                        AUTH, USER);
+        archRule.check(javaClasses);
+
+    }
+
+    @Test
+    @DisplayName("Test for NewsLetter and Notifications Module")
+    void givenNewsLetterModule_thenCheckDependencies() {
+        //Import classes from application and omit test classes
+        JavaClasses javaClasses = new ClassFileImporter(new ImportOptions()
+                .with(new ImportOption.DoNotIncludeTests())).importPackages(HUB_EVENT);
+
+        ArchRule archRule = classes()
+                .that()
+                .resideInAPackage(NEWSLETTER)
+                .should().onlyDependOnClassesThat()
+                .resideInAnyPackage(JAVA, JAVAX, SPRING,
+                        EVENTS, USER, MAIL_MODULE, NEWSLETTER);
+        archRule.check(javaClasses);
+
+    }
+
+    @Test
+    @DisplayName("Test for Mail Module")
+    void givenMailModule_thenCheckDoesNotDependOnAnyOtherModuleFromApp() {
+        //Import classes from application and omit test classes
+        JavaClasses javaClasses = new ClassFileImporter(new ImportOptions()
+                .with(new ImportOption.DoNotIncludeTests())).importPackages(HUB_EVENT);
+
+        ArchRule archRule = classes()
+                .that()
+                .resideInAPackage(MAIL_MODULE)
+                .should().onlyDependOnClassesThat()
+                .resideInAnyPackage(JAVA, JAVAX, SPRING,
+                        MAIL_MODULE);
+        archRule.check(javaClasses);
+
+    }
+
+    @Test
+    @DisplayName("Test for User API Module")
+    void givenUserAPIModule_thenCheckDependencies() {
+        //Import classes from application and omit test classes
+        JavaClasses javaClasses = new ClassFileImporter(new ImportOptions()
+                .with(new ImportOption.DoNotIncludeTests())).importPackages(HUB_EVENT);
+
+        ArchRule archRule = classes()
+                .that()
+                .resideInAPackage(USER_API)
+                .should().onlyDependOnClassesThat()
+                .resideInAnyPackage(JAVA, JAVAX, SPRING,
+                        NEWSLETTER,EVENTS,USER,AUTH, USER_API);
+        archRule.check(javaClasses);
+
+    }
+
+    @Test
+    @DisplayName("Test for Authentication Module")
+    void givenAuthModule_thenCheckDoesNotDependOnAnyOtherModuleFromApp() {
+        //Import classes from application and omit test classes
+        JavaClasses javaClasses = new ClassFileImporter(new ImportOptions()
+                .with(new ImportOption.DoNotIncludeTests())).importPackages(HUB_EVENT);
+
+        ArchRule archRule = classes()
+                .that()
+                .resideInAPackage(AUTH)
                 .should().onlyDependOnClassesThat()
                 .resideInAnyPackage(JAVA, JAVAX, SPRING,
                         AUTH);
@@ -101,74 +181,18 @@ class ArchTest {
     }
 
     @Test
-    @DisplayName("Test for NewsLetter and Notifications Module")
-    void givenNewsLetterModule_thenCheckDependencies() {
-        JavaClasses javaClasses = new ClassFileImporter().importPackages(HUB_EVENT);
-
-        ArchRule archRule = classes()
-                .that()
-                .resideInAPackage(NEWSLETTER)
-                .should().onlyDependOnClassesThat()
-                .resideInAnyPackage(JAVA, JAVAX, SPRING,
-                        EVENTS, USER, MAIL_MODULE);
-        archRule.check(javaClasses);
-
-    }
-
-    @Test
-    @DisplayName("Test for Mail Module")
-    void givenMailModule_thenCheckDoesNotDependOnAnyOtherModuleFromApp() {
-        JavaClasses javaClasses = new ClassFileImporter().importPackages(HUB_EVENT);
-
-        ArchRule archRule = classes()
-                .that()
-                .resideInAPackage(MAIL_MODULE)
-                .should().onlyDependOnClassesThat()
-                .resideInAnyPackage(JAVA, JAVAX, SPRING);
-        archRule.check(javaClasses);
-
-    }
-
-    @Test
-    @DisplayName("Test for User API Module")
-    void givenUserAPIModule_thenCheckDependencies() {
-        JavaClasses javaClasses = new ClassFileImporter().importPackages(HUB_EVENT);
-
-        ArchRule archRule = classes()
-                .that()
-                .resideInAPackage(USER_API)
-                .should().onlyDependOnClassesThat()
-                .resideInAnyPackage(JAVA, JAVAX, SPRING,
-                        NEWSLETTER,EVENTS,USER,AUTH);
-        archRule.check(javaClasses);
-
-    }
-
-    @Test
-    @DisplayName("Test for Authentication Module")
-    void givenAuthModule_thenCheckDoesNotDependOnAnyOtherModuleFromApp() {
-        JavaClasses javaClasses = new ClassFileImporter().importPackages(HUB_EVENT);
-
-        ArchRule archRule = classes()
-                .that()
-                .resideInAPackage(AUTH)
-                .should().onlyDependOnClassesThat()
-                .resideInAnyPackage(JAVA, JAVAX, SPRING);
-        archRule.check(javaClasses);
-
-    }
-
-    @Test
     @DisplayName("Test for Admin API Module")
     void givenAdminAPIModule_thenCheckDependencies() {
-        JavaClasses javaClasses = new ClassFileImporter().importPackages(HUB_EVENT);
+        //Import classes from application and omit test classes
+        JavaClasses javaClasses = new ClassFileImporter(new ImportOptions()
+                .with(new ImportOption.DoNotIncludeTests())).importPackages(HUB_EVENT);
 
         ArchRule archRule = classes()
                 .that()
                 .resideInAPackage(ADMIN_API)
                 .should().onlyDependOnClassesThat()
                 .resideInAnyPackage(JAVA, JAVAX, SPRING,
-                        USER,AUTH,STATS);
+                        USER,AUTH,STATS, ADMIN_API);
         archRule.check(javaClasses);
 
     }
@@ -176,14 +200,16 @@ class ArchTest {
     @Test
     @DisplayName("Test for Statistics Module")
     void givenStatsModule_thenCheckDependencies() {
-        JavaClasses javaClasses = new ClassFileImporter().importPackages(HUB_EVENT);
+        //Import classes from application and omit test classes
+        JavaClasses javaClasses = new ClassFileImporter(new ImportOptions()
+                .with(new ImportOption.DoNotIncludeTests())).importPackages(HUB_EVENT);
 
         ArchRule archRule = classes()
                 .that()
                 .resideInAPackage(STATS)
                 .should().onlyDependOnClassesThat()
                 .resideInAnyPackage(JAVA, JAVAX, SPRING,
-                        SCRAPERS,EVENTS,USER);
+                        SCRAPERS,EVENTS,USER, STATS);
         archRule.check(javaClasses);
 
     }
